b9a8539f60477f84c97899b8a59299296c4cb162
Amarjeet kumar <amarjeetkumart@outlook.com>
2026-02-10 16:40:14 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    axios.get("http://YOUR_EC2_IP:5000/api/count")
+    axios.get("http://13.126.242.155:5000/api/count")
       .then(res => setCount(res.data.count))
       .catch(err => console.log(err));
   }, []);
